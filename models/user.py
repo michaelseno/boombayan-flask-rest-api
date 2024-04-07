bf0c845b282b8b209403661c23d8a55e74b6d409
Michael Jordan Seno <mjseno@Michaels-MacBook-Pro-2.local>
2024-04-07 21:12:12 +0800

    Implemented user, login, register endpoints

---
+++ models/user.py
@@ -0,0 +1,14 @@
+from db import db
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    firstname = db.Column(db.String(80), unique=False, nullable=False)
+    lastname = db.Column(db.String(80), unique=False, nullable=False)
+    bank_name = db.Column(db.String(80), unique=False, nullable=False)
+    bank_number = db.Column(db.Integer, unique=True, nullable=False)
+
