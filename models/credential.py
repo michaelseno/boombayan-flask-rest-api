594379b87a280073aab7ca7c38834c0e4d9a224e
Michael Jordan Seno <mjseno@Michaels-MacBook-Pro-2.local>
2024-04-12 07:52:28 +0800

    added bank schema and implementation

--- models/credential.py
+++ models/credential.py
@@ -4,11 +4,10 @@ from db import db
 class CredentialModel(db.Model):
     __tablename__ = "credentials"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(), primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
     is_admin = db.Column(db.Boolean(), nullable=False)
-
     users = db.relationship("UserModel", back_populates="credentials")
 
 
--- schemas.py
+++ schemas.py
@@ -2,10 +2,17 @@ from marshmallow import Schema, fields
 
 
 class PlainCredentialSchema(Schema):
-    id = fields.Int(dump_only=True)
+    id = fields.Str(dump_only=True)
     username = fields.Str(required=True)
     password = fields.Str(required=True)
     is_admin = fields.Bool(dump_only=True)
+
+
+class PlainBankSchema(Schema):
+    id = fields.Str(dump_only=True)
+    bank_name = fields.Str(required=True)
+    account_number = fields.Str(required=True)
+    is_active = fields.Bool(dump_only=True)
 
 
 class PlainUserSchema(Schema):
@@ -13,17 +20,22 @@ class PlainUserSchema(Schema):
     firstname = fields.Str(required=True)
     lastname = fields.Str(required=True)
     email = fields.Str(required=True)
-    phone = fields.Int(required=True)
+    phone = fields.Str(required=True)
     status = fields.Str(dump_only=True)
 
 
 class CredentialSchema(PlainCredentialSchema):
-    cred_id = fields.Int(required=True, load_only=True)
+    cred_id = fields.Int(required=True, dump_only=True)
     user = fields.Nested(PlainUserSchema(), dump_only=True)
 
 
 class UserSchema(PlainUserSchema):
-    user_id = fields.Int(required=True, load_only=True)
+    user_id = fields.Str(required=True, dump_only=True)
+    credentials = fields.Nested(PlainCredentialSchema(), load_only=True)
+    bank = fields.Nested(PlainBankSchema(), load_only=True)
+
+
+class UserDisplaySchema(PlainUserSchema):
+    user_id = fields.Str(required=True, dump_only=True)
     credentials = fields.Nested(PlainCredentialSchema(), dump_only=True)
-
-
+    banks = fields.Nested(PlainBankSchema(), dump_only=True)
